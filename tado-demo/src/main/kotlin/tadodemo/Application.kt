e67ce10e985f299f37991faaf18f74e4e1dc4a68
Kristel <kristel.nieuwenhuijs@kramp.com>
2024-08-03 14:50:47 +0200

    close app after run

--- tado-demo/src/main/kotlin/tadodemo/Application.kt
+++ tado-demo/src/main/kotlin/tadodemo/Application.kt
@@ -7,5 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 open class Application
 
     fun main(args: Array<String>) {
-        SpringApplication.run(Application::class.java, *args)
+        SpringApplication
+            .run(Application::class.java, *args)
+            .close()
     }
